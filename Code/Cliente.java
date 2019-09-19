8d1a001522e48a51ef9112bac4c24cfb5cc2c726
Samuel <samuelpedro1999@gmail.com>
2019-09-19 19:00:18 -0300

    Comecei

---
+++ Code/Cliente.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Cliente{
+  private static String Nome;
+  private static int Codigo;
+  private static String CPF;
+  private ArrayList<String> Enderecos = new ArrayList<String>();
+  private static String Email;
+  private static String Senha;
+}
