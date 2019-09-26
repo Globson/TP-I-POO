501fecb4505c20a6655717d1f5a1c1349090ff73
Samuel <samuelpedro1999@gmail.com>
2019-09-26 17:57:56 -0300

    removi arquivos do vscode

--- Code/Produto.java
+++
@@ -1,8 +0,0 @@
-public class Produto{
-  private static int CodigoProduto;
-  private static int Quantidade;
-  private static int preco;
-  private static String Descricao;
-  private static String Categoria;
-
-}
